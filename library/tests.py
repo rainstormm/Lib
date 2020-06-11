eedf9a0656e6bbb53c3dee5445753cd37dd12845
rainstromm <ls4900@nyu.edu>
2020-06-11 12:30:14 -0400

    updated

--- library/tests.py
+++ library/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from library.models import Book
+import requests
+from library.views import book_detail
 
 # Create your tests here.
 class IndexPageTest(TestCase):
@@ -10,7 +12,11 @@ class IndexPageTest(TestCase):
         self.assertTemplateUsed(template_name='index.html')
 
 class BookSearchPageTest(TestCase):
-
+    def setUp(self) -> None:
+        Book.objects.create(ISBN="11111111",title="title1",author="author1",press="press1",quantity=5)
+    def test_search(self):
+        response = self.client.get("/search/?search_by=Title&keyword=title1")
+        self.assertEqual(response.content, b'there is no such an ISBN')
     def test_search_page(self):
         response = self.client.get('/search/')
         self.assertEqual(response.status_code,200)
@@ -23,8 +29,14 @@ class BookDetailPageTest(TestCase):
         Book.objects.create(ISBN="11111111",title="title1",author="author1",press="press1",quantity=5)
     def test_ISBN(self):
         book=Book.objects.get(ISBN="11111111")
-        self.assertEqual(book.ISBN, '11111111')
-
+        self.assertEqual(book.title, 'title1')
+        self.assertEqual(book.author, 'author1')
+        self.assertEqual(book.quantity, 5)
+    def test_no_ISBN(self):
+        response = self.client.get('/book/detail?ISBN=None')
+        self.assertEqual(response.content, b'there is no such an ISBN')
+        response = self.client.get('/book/detail?ISBN=22222222')
+        self.assertEqual(response.content, b'there is no such an ISBN')
     def test_detail_page(self):
         response = self.client.get('/book/detail')
         self.assertEqual(response.status_code,200)
